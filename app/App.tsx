8af438602013dfaded72a0a1b6d4bb2209045427
Anthony bassey <a.bassey@live.co.uk>
2020-11-14 21:47:24 +0100

    fixing safe area issues

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,6 @@
 import 'react-native-gesture-handler'
 import React from 'react'
-import {StatusBar} from 'react-native'
-import {SafeAreaProvider} from 'react-native-safe-area-context'
+import {SafeAreaView, StatusBar, View} from 'react-native'
 import {NavigationContainer} from '@react-navigation/native'
 import {colors} from './ui/style/colors'
 import {navigationService} from './ui/navigation/Navigation.service'
@@ -9,11 +8,14 @@ import {Navigation} from './ui/navigation/Navigation'
 
 export const App = () => {
   return (
-    <SafeAreaProvider>
+    <>
+      <SafeAreaView style={{flex: 0, backgroundColor: colors.yomentoNavy}} />
       <StatusBar backgroundColor={colors.yomentoNavy} barStyle="light-content" />
-      <NavigationContainer ref={navigationRef => (navigationService.navigator = navigationRef)}>
-        <Navigation />
-      </NavigationContainer>
-    </SafeAreaProvider>
+      <SafeAreaView style={{flex: 1, backgroundColor: colors.yomentoPurple}}>
+        <NavigationContainer ref={navigationRef => (navigationService.navigator = navigationRef)}>
+          <Navigation />
+        </NavigationContainer>
+      </SafeAreaView>
+    </>
   )
 }
